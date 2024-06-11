ae9838482a111a558a98b1b83be0e175460ca77b
Alexandr <a.salauyou@arateg.com>
2024-06-11 20:59:00 +0300

    add Kolya krasavchik

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Vasya krasavchik");
+        System.out.println("Kolya krasavchik");
     }
 }
